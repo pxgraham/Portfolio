984445c1e4d95eef876c03ae5a3edaa7a1b09048
pxgraham <pxgraham@gmail.com>
2019-12-25 10:55:22 -0700

    basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
         <Switch>
           <Route exact path="/" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
