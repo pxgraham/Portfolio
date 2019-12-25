ef80ac7b7976c8179a23c9da0f185005d9778b64
pxgraham <pxgraham@gmail.com>
2019-12-25 11:51:32 -0700

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, HashRouter } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -8,13 +8,13 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router basename="/">
+    <HashRouter>
         <Switch>
           <Route exact path="/" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
           <Route component={Portfolio} />
         </Switch>
-     </Router>
+     </HashRouter>
   );
 }
 
