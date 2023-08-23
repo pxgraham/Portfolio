c925b9d156bfc3af0ca098c4998c1ec3b4dfe78d
pxgraham <41251008+pxgraham@users.noreply.github.com>
2023-08-23 06:00:53 -0700

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,24 +13,6 @@ class Navbar extends Component {
             <a href="https://github.com/pxgraham" target="_blank">
               <i className="fab fa-github"></i>
             </a>
-            <a href="https://linkedin.com/in/peyton-graham-0b268b167" target="_blank">            
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://www.reddit.com/user/Sleysin" target="_blank">            
-              <i className="fab fa-reddit"></i>
-            </a>
-            <a href="https://discord.gg/TfvvVb" target="_blank">              
-              <i className="fab fa-discord"></i>
-            </a>
-            <a href="https://www.youtube.com/channel/UCYY7OFCBCAAWb7QOHnQ776w" target="_blank">              
-              <i className="fab fa-youtube"></i>
-            </a>
-            <a href="mailto:pxgraham@gmail.com" target="_blank">              
-              <i className="fas fa-envelope"></i>
-            </a>
-            <a href="tel:9283586658">              
-              <i className="fas fa-phone-square-alt"></i>
-            </a>
           </nav>     
         </div>
     )
