2add0c13faea5e715128180bbad0826b96558fa8
Peyton <pxgraham@gmail.com>
2025-04-01 17:53:50 -0700

    removed footer hrefs

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark text-center text-light">
-        <span>Sponsored in part by</span>
-        <a className="navbar-brand text-primary" href="https://sleysin.com">Sleysin Games</a>
-        <a className="navbar-brand text-primary" href="https://hiddenblueinside.com">The Hidden Blue Shop</a>
-        <a className="navbar-brand text-primary" href="https://pxgraham.github.io">But Why Pod</a>
+        <a className="navbar-brand text-primary" href="https://pxgraham.github.io">https://pxgraham.github.io</a>
       </nav>
     )
   }
